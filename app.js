e43fead7b47015df3870ea1c45bfe768b0839a01
ShivasunuGupta <shivasunugupta@gmail.com>
2025-07-11 11:07:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,87 +1,3 @@
-// const express = require("express");
-// const socket = require("socket.io");
-// const http = require("http");
-// const {Chess} = require("chess.js");
-// const path = require("path");
-// const { disconnect } = require("process");
-
-// const app = express();
-
-// const server = http.createServer(app);
-// const io =socket(server);
-
-// const chess = new Chess();           //Chess.js ka object- consists all the rules 
-
-// let players ={};
-// let currentPlayer ="w";
-
-// app.set("view engine","ejs");
-// app.use(express.static(path.join(__dirname,"public")));
-
-// app.get("/",(req,res) => {
-//     res.render("index",{title:"Chess Game"});
-// });
-
-// //call back function - decides that how many clients 
-// // recieved the massage from server
-// io.on("connection" , function(uniquesocket){
-//     console.log("connected");
-
-//     if(!players.white){
-//         players.white = uniquesocket.id;
-//         uniquesocket.emit("playerRole" ,"w");
-//     } else if(!players.black){
-//         players.black = uniquesocket.id;
-//         uniquesocket.emit("playerRole" , "b");
-//     } else {
-//         uniquesocket.emit("spectatorRole");
-//     }
-
-
-//     uniquesocket.on("disconnect" , function (){
-//         if(uniquesocket.id == players.white){
-//             delete players.white;
-//         }
-//         else if(uniquesocket.id == players.black){
-//             delete players.black;
-//         }
-//     });
-
-//     uniquesocket.on("move",(move) =>{
-//         try{
-//             if(chess.turn() == 'w' && uniquesocket.id != players.white){return};
-//             if(chess.turn() == 'b' && uniquesocket.id != players.black){return};
-
-//             const result = chess.move(move);
-//             if(result){
-//                 currentPlayer = chess.turn();
-//                 io.emit("move",move);
-//                 io.emit("boardState" , chess.fen())
-
-//             } else {
-//                 console.log("Invalid move :" , move);
-//                 uniquesocket.emit("invalidMove",move);
-//             }
-//         }
-//         catch(err){
-//             console.log(err);
-//             uniquesocket.emit("Invalid move :" , move);
-//         }
-
-//     });
-// });
-
-// server.listen(3000 ,function(){
-//     console.log("listening on server 3000");
-// });
-
-
-
-
-
-
-
-
 const express = require("express");
 const socket = require("socket.io");
 const http = require("http");
@@ -91,7 +7,7 @@ const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const io = socket(server);
-const chess = new Chess();
+const chess = new Chess();        //Chess.js ka object- consists all the rules 
 
 let players = {};
 let playerNames = {};
@@ -104,6 +20,9 @@ app.get("/", (req, res) => {
   res.render("index", { title: "Multiplayer Chess" });
 });
 
+
+//call back function - decides that how many clients 
+// recieved the massage from server
 io.on("connection", (socket) => {
   console.log("New user connected:", socket.id);
 
@@ -137,7 +56,6 @@ io.on("connection", (socket) => {
     if (result) {
       io.emit("move", move);
       io.emit("boardState", chess.fen());
-      io.emit("moveHistory", chess.history({ verbose: false }));
     }
   });
 
